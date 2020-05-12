16f24e433fc37821930535eb6313ca42559221f2
arb712 <ariqbimantoro@gmail.com>
2020-05-12 14:46:57 +0700

    add carousel for category

--- src/pages/Home/Carousel/Carousel.js
+++ src/pages/Home/Carousel/Carousel.js
@@ -1,63 +1,37 @@
 import React, { Component } from "react";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { Carousel } from "react-responsive-carousel";
-
+import Carousel, { Dots } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import {Button} from 'react-bootstrap';
 import { Container } from "react-bootstrap";
 
-// import music from "../../../image/music.jpg";
-// import art from "../../../image/art.jpeg";
-// import film from "../../../image/film.jpeg";
-// import fashion from "../../../image/fashion.jpeg";
-// import sport from "../../../image/sport.jpeg";
-// import festival from "../../../image/festival.jpeg";
-// import nightlife from "../../../image/nightlife.jpeg";
-// import charity from "../../../image/charity.jpg";
+import music from "../../../image/music.jpg";
+import art from "../../../image/art.jpeg";
+import film from "../../../image/film.jpeg";
+import fashion from "../../../image/fashion.jpeg";
+import sport from "../../../image/sport.jpeg";
+import festival from "../../../image/festival.jpeg";
+import nightlife from "../../../image/nightlife.jpeg";
+import charity from "../../../image/charity.jpg";
 
 import "./Carousel.css";
 
-class Slider extends Component {
+export default class CategoryCarousel extends Component {
   render() {
     return (
       <div>
-        <Container>
-          {/* <Carousel>
-            <div>
-              <img src={music} className="carousel-image" />
-              <p className="legend">Music</p>
-            </div>
-            <div>
-              <img src={art} className="carousel-image" />
-              <p className="legend ">Visual Arts</p>
-            </div>
-            <div>
-              <img src={film} className="carousel-image" />
-              <p className="legend">Film</p>
-            </div>
-            <div>
-              <img src={fashion} className="carousel-image" />
-              <p className="legend">Fashion</p>
-            </div>
-            <div>
-              <img src={sport} className="carousel-image" />
-              <p className="legend">Sports</p>
-            </div>
-            <div>
-              <img src={festival} className="carousel-image" />
-              <p className="legend">Festival</p>
-            </div>
-            <div>
-              <img src={nightlife} className="carousel-image" />
-              <p className="legend">Nightlife</p>
-            </div>
-            <div>
-              <img src={charity} className="carousel-image" />
-              <p className="legend">Charity & Fairs</p>
-            </div>
-          </Carousel> */}
+        <Container className="containerCarousel">
+          <Carousel centered infinite arrows slidesPerPage={3}>
+            <a href="#" className="text-center"><img src={music} className="carouselImg"/><Button className="buttonImg">Music</Button></a>
+            <a href="#" className="text-center"><img src={art} className="carouselImg"/><Button className="buttonImg">Art</Button></a>
+            <a href="#" className="text-center"><img src={film} className="carouselImg"/><Button className="buttonImg">Film</Button></a>
+            <a href="#" className="text-center"><img src={fashion} className="carouselImg"/><Button className="buttonImg">Fashion</Button></a>
+            <a href="#" className="text-center"><img src={sport} className="carouselImg"/><Button className="buttonImg">Sport</Button></a>
+            <a href="#" className="text-center"><img src={festival} className="carouselImg"/><Button className="buttonImg">Festival</Button></a>
+            <a href="#" className="text-center"><img src={nightlife} className="carouselImg"/><Button className="buttonImg">Nightlife</Button></a>
+            <a href="#" className="text-center"><img src={charity} className="carouselImg"/><Button className="buttonImg">Charity</Button></a>
+          </Carousel>
         </Container>
       </div>
     );
   }
 }
-
-export default Slider;
