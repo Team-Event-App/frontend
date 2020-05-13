51c65d9955a68df7fd1ab7dbf4ab37ee3e48aa79
arb712 <ariqbimantoro@gmail.com>
2020-05-14 02:02:12 +0700

    update a little css styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,98 +1,93 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {useHistory} from 'react-router-dom'
-import {logout} from '../../actions/loginActions';
-import {connect} from 'react-redux'
+import { useHistory } from "react-router-dom";
+import { logout } from "../../actions/loginActions";
+import { connect } from "react-redux";
 
 import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
 
 import "./Navbar.css";
 import Logo from "./../../image/logo.png";
 
-const Navbars= (props) =>{
+const Navbars = (props) => {
   let privateRoute;
-  const [data,setData] = useState();
-  const[viewLogin,setViewLogin] = useState();
+  const [data, setData] = useState();
+  const [viewLogin, setViewLogin] = useState();
   const history = useHistory();
   const logOut = () => {
     props.logout();
-    history.push("/login");
-  }
-  useEffect(()=>{
-    if(props.viaLogin){
-      privateRoute = (
+    history.push("/");
+  };
+  useEffect(() => {
+    if (props.viaLogin) {
+      privateRoute = <></>;
+      setViewLogin(
         <>
-                    <NavDropdown
-              className="mr-5 pr-4 ml-4"
-              title={
-                <span>
-                  <i className="fa fa-user fa-fw"></i>Profile
-                </span>
-              }
-            >
-              <NavDropdown.Item>
-                <i className="fas fa-envelope fa-fw"></i> User Profile
-              </NavDropdown.Item>
+          <NavDropdown
+            className="mr-5 pr-4 ml-4"
+            title={
+              <span>
+                <i className="fa fa-user fa-fw"></i>Profile
+              </span>
+            }
+          >
+            <NavDropdown.Item className="navDropItem">
+              <i className="fas fa-envelope fa-fw"></i> User Profile
+            </NavDropdown.Item>
 
-              <NavDropdown.Item>
-                <i className="fas fa-bookmark mr-2"></i>Bookmark
-              </NavDropdown.Item>
+            <NavDropdown.Item className="navDropItem">
+              <i className="fas fa-bookmark mr-2"></i>Bookmark
+            </NavDropdown.Item>
 
-              <NavDropdown.Item>
-                <i className="far fa-clock mr-2"></i>History Purchase
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-            </NavDropdown>
+            <NavDropdown.Item className="navDropItem">
+              <i className="far fa-clock mr-2"></i>History Purchase
+            </NavDropdown.Item>
+            <NavDropdown.Item className="navDropItem">
+              <Link className="fas fa-sign-out-alt i-logout" onClick={logOut}></Link>{" "}
+              Logout
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+          </NavDropdown>
         </>
-      )
-      setViewLogin(              <NavDropdown.Item>
-        <Link className="fas fa-sign-out-alt " onClick={logOut}></Link> Logout
-      </NavDropdown.Item>)
-      setData(privateRoute)
-    }else{
+      );
+      setData(privateRoute);
+    } else {
       setData();
-      setViewLogin(            <Button className="signInButton btn both-line-dark" href="/login">
-      Sign In
-      <i class="fas fa-sign-in-alt ml-2"></i>
-    </Button>)
+      setViewLogin(
+        <Button className="signInButton btn both-line-dark" href="/login">
+          Sign In
+          <i class="fas fa-sign-in-alt ml-2"></i>
+        </Button>
+      );
     }
-  },[props.viaLogin])
-    return (
-      <Navbar
-        bg="white"
-        variant="light"
-        expand="lg"
-        className="navbar fixed-top"
-      >
-        <Link to="/" className="ml-3 brand">
-          <img src={Logo} alt="logo" />
-        </Link>
-        <Navbar.Toggle aria-controls="toogle" />
-        <Navbar.Collapse id="toogle">
-          <Nav className="ml-auto">
-            <Button
-              className="signInButton mr-3 btn both-line-dark"
-              href="/event"
-            >
-              <i class="fas fa-plus mr-2"></i>
-              Create Events
-            </Button>
-          </Nav>
-          <Nav>
-            {data}
-          </Nav>
-          <Nav>
-            {viewLogin}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-}
+  }, [props.viaLogin]);
+  return (
+    <Navbar bg="white" variant="light" expand="lg" className="navbar fixed-top">
+      <Link to="/" className="ml-3 brand">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <Navbar.Toggle aria-controls="toogle" />
+      <Navbar.Collapse id="toogle">
+        <Nav className="ml-auto">
+          <Button
+            className="signInButton mr-3 btn both-line-dark"
+            href="/event"
+          >
+            <i class="fas fa-plus mr-2"></i>
+            Create Events
+          </Button>
+        </Nav>
+        <Nav>{data}</Nav>
+        <Nav>{viewLogin}</Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-const mapStateToProps= (state) => {
-  return{
-    viaLogin:state.login.viaLogin
-  }
-}
-const mapDispatchToProps = {logout}
-export default connect(mapStateToProps,mapDispatchToProps)(Navbars);
+const mapStateToProps = (state) => {
+  return {
+    viaLogin: state.login.viaLogin,
+  };
+};
+const mapDispatchToProps = { logout };
+export default connect(mapStateToProps, mapDispatchToProps)(Navbars);
--- src/pages/Create_Event/CreateEvent.js
+++ src/pages/Create_Event/CreateEvent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import {
   Container,
@@ -16,186 +16,165 @@ import "./CreateEvent.css";
 import Navbars from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
-class createEvent extends Component {
-  render() {
-    return (
-      <div className="mt-5 pt-5">
-        <Navbars />
-        <Container>
-          <Row>
-            <Col>
-              <h2 className="text-center pt-5 mb-4 titleCategory">
-                Create Event
-              </h2>
-              <div className="underlineEvent mx-auto"></div>
-            </Col>
-          </Row>
-          <Form className="justify-content-center align-items-center">
-            <Form.Row>
-              <Container>
-                <Form.Group as={Col} md={6} controlId="formBasicEmail">
-                  <Form.Label className="subLabel pb-2">Event Info</Form.Label>
+const Index = () => {
+  const [data,setData] = useState({
+    title:"",
+  })
+  return(
+    <div className="mt-5 pt-5">
+    <Navbars />
+    <Container>
+      <Row>
+        <Col>
+          <h2 className="text-center pt-5 mb-4 titleCategory">
+            Create Event
+          </h2>
+          <div className="underlineEvent mx-auto"></div>
+        </Col>
+      </Row>
+      <Form className="justify-content-center align-items-center">
+        <Form.Row>
+          <Container>
+            <Form.Group as={Col} md={6} controlId="formBasicEmail">
+              <Form.Label className="subLabel pb-2">Event Info</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Title"
+                className="inputText"
+                name="title"
+              />
+              <Form.Text className="text-muted">
+                Your Event Main Title.
+              </Form.Text>
+            </Form.Group>
+            <Form.Row className="pl-3">
+              <Form.Group as={Col} md={2}>
+                <Form.Label>Category</Form.Label>
+                <Form.Control
+                  as="select"
+                  value="Choose..."
+                  className="inputText"
+                  name="category"
+                >
+                  <option>Charity</option>
+                  <option>Art</option>
+                  <option>Film</option>
+                  <option>Music</option>
+                  <option>Festival</option>
+                  <option>Fashion</option>
+                  <option>Sport</option>
+                  <option>Nightlife</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group as={Col} md={2}>
+                <Form.Label>People</Form.Label>
+                <Form.Control
+                  type="text"
+                  className="inputText"
+                  placeholder="Max People"
+                  name="limitPeople"
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row className="pl-3">
+              <Form.Group as={Col} md={2.5} controlId="formBasicEmail">
+                <Form.Label>Organizer Event</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Organizer Event"
+                  className="inputText"
+                  name="organizerName"
+                />
+              </Form.Group>
+              <Form.Group as={Col} md={3} controlId="formBasicEmail">
+                <Form.Label>Penanggung Jawab Event</Form.Label>
+                <Form.Control type="text" className="inputText" name="responsibleName"/>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row className="pl-3">
+              <Form.Group
+                as={Col}
+                md={5}
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Description Event</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows="3"
+                  className="inputText"
+                  style={{ width: "33rem" }}
+                  name="description"
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Label className="subLabel pl-3">Location</Form.Label>
+            <Form.Row className="pl-3">
+              <Form.Group as={Col} md={6} controlId="formBasicEmail">
+                <Form.Label className="pb-2">Venue Location</Form.Label>
+                <Form.Control type="text" className="inputText" name="location"/>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row className="pl-3">
+              <Form.Group as={Col} md={6} controlId="formBasicEmail">
+                <Form.Label className="subLabel">
+                  Detail Event
+                </Form.Label>
+              </Form.Group>
+            </Form.Row>
+            <Form.Row className="pl-4">
+              <Form.Group>
+                <Form.Label>Price</Form.Label>
+                <InputGroup>
+                  <InputGroup.Prepend>
+                    <InputGroup.Text
+                      id="inputGroupPrepend"
+                      className="inputPrepand"
+                    >
+                      Rp.
+                    </InputGroup.Text>
+                  </InputGroup.Prepend>
                   <Form.Control
-                    type="email"
-                    placeholder="Title"
+                    type="text"
+                    aria-describedby="inputGroupPrepend"
+                    name="price"
                     className="inputText"
                   />
-                  <Form.Text className="text-muted">
-                    Your Event Main Title.
-                  </Form.Text>
-                </Form.Group>
-                <Form.Row className="pl-3">
-                  <Form.Group as={Col} md={2}>
-                    <Form.Label>Category</Form.Label>
-                    <Form.Control
-                      as="select"
-                      value="Choose..."
-                      className="inputText"
-                    >
-                      <option></option>
-                      <option>...</option>
-                    </Form.Control>
-                  </Form.Group>
-                  <Form.Group as={Col} md={2}>
-                    <Form.Label>People</Form.Label>
-                    <Form.Control
-                      className="inputText"
-                      placeholder="Max People"
-                    />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Row className="pl-3">
-                  <Form.Group as={Col} md={2.5} controlId="formBasicEmail">
-                    <Form.Label>Organizer Event</Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="Organizer Event"
-                      className="inputText"
-                    />
-                  </Form.Group>
-                  <Form.Group as={Col} md={3} controlId="formBasicEmail">
-                    <Form.Label>Penanggung Jawab Event</Form.Label>
-                    <Form.Control type="email" className="inputText" />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Row className="pl-3">
-                  <Form.Group
-                    as={Col}
-                    md={5}
-                    controlId="exampleForm.ControlTextarea1"
-                  >
-                    <Form.Label>Description Event</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      rows="3"
-                      className="inputText"
-                      style={{ width: "33rem" }}
-                    />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Label className="subLabel pl-3 pb-2">Location</Form.Label>
-                <Form.Row className="pl-3 " as={Col} md={8}>
-                  <ButtonGroup toggle className="pl-2">
-                    <ToggleButton
-                      type="radio"
-                      name="online"
-                      defaultChecked
-                      value="1"
-                      className="toggleButton"
-                    >
-                      Online
-                    </ToggleButton>
-                    <ToggleButton
-                      type="radio"
-                      name="offline"
-                      value="2"
-                      className="toggleButton"
-                    >
-                      Offline
-                    </ToggleButton>
-                    <ToggleButton
-                      type="radio"
-                      name="tbd"
-                      value="3"
-                      className="toggleButton"
-                    >
-                      TBD
-                    </ToggleButton>
-                  </ButtonGroup>
-                </Form.Row>
-                <Form.Row className="pl-3 pt-3">
-                  <Form.Group as={Col} md={6} controlId="formBasicEmail">
-                    <Form.Label className="pb-2">Venue Location</Form.Label>
-                    <Form.Control type="email" className="inputText" />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Row className="pl-3">
-                  <Form.Group as={Col} md={6} controlId="formBasicEmail">
-                    <Form.Label className="subLabel pb-2">
-                      Detail Event
-                    </Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="Title"
-                      className="inputText"
-                    />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Row className="pl-4">
-                  <Form.Group>
-                    <Form.Label>Price</Form.Label>
-                    <InputGroup>
-                      <InputGroup.Prepend>
-                        <InputGroup.Text
-                          id="inputGroupPrepend"
-                          className="inputPrepand"
-                        >
-                          Rp.
-                        </InputGroup.Text>
-                      </InputGroup.Prepend>
-                      <Form.Control
-                        type="text"
-                        aria-describedby="inputGroupPrepend"
-                        name="price"
-                        className="inputText"
-                      />
-                      <input
-                        type="date"
-                        className="pl-3 ml-3 inputText"
-                      ></input>
-                    </InputGroup>
-                  </Form.Group>
-                </Form.Row>
-                <Form.Row className="pl-4">
-                  <Form.Group className="inputText">
-                    <Form.File id="custom-file" label="Upload Image" custom />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Row className="pl-3">
-                  <Form.Group
-                    as={Col}
-                    md={6}
-                    controlId="exampleForm.ControlTextarea1"
-                  >
-                    <Form.Label>Detail Event</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      rows="3"
-                      className="inputText"
-                    />
-                  </Form.Group>
-                </Form.Row>
-                <Form.Group className="pl-3">
-                  <Button className="save">Save</Button>
-                </Form.Group>
-              </Container>
+                  <input
+                    type="date"
+                    className="pl-3 ml-3 inputText"
+                  ></input>
+                </InputGroup>
+              </Form.Group>
             </Form.Row>
-          </Form>
-        </Container>
-        <Footer />
-      </div>
-    );
-  }
+            <Form.Row className="pl-4">
+              <Form.Group className="inputText">
+                <Form.File id="custom-file" label="Upload Image" custom />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row className="pl-3">
+              <Form.Group
+                as={Col}
+                md={6}
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Detail Event</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows="3"
+                  className="inputText"
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Group className="pl-3">
+              <Button className="save">Save</Button>
+            </Form.Group>
+          </Container>
+        </Form.Row>
+      </Form>
+    </Container>
+    <Footer />
+  </div>
+  )
 }
 
-export default createEvent;
+export default Index;
