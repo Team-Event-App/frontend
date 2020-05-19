0116e2e8d8a197de7c8197dd2e7f37510a5d74c6
EvanCharles16 <evancharles1609@gmail.com>
2020-05-19 21:22:51 +0700

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,88 +11,88 @@ import "./Navbar.css";
 import Logo from "./../../image/logo.png";
 
 const Navbars = (props) => {
-  const [data, setData] = useState();
-  const [viewLogin, setViewLogin] = useState();
-  const history = useHistory();
-  const logOut = () => {
-    props.logout();
-    history.push("/");
-  };
-  useEffect(() => {
-    if (props.viaLogin) {
-      setViewLogin(
-        <>
-          <NavDropdown
-            className="mr-5 pr-4 ml-4"
-            title={
-              <span>
-                <i className="fa fa-user fa-fw"></i>Profile
-              </span>
-            }
-          >
-            <NavDropdown.Item className="navDropItem">
-              <Link to="/profile" className="profileSetting">
-                <i className="fas fa-envelope fa-fw"></i> User Profile
-              </Link>
-            </NavDropdown.Item>
-
-            {/* <NavDropdown.Item className="navDropItem">
+	const [data, setData] = useState();
+	const [viewLogin, setViewLogin] = useState();
+	const history = useHistory();
+	const logOut = () => {
+		props.logout();
+		history.push("/");
+	};
+	useEffect(() => {
+		if (props.viaLogin) {
+			setViewLogin(
+				<>
+					<NavDropdown
+						className="mr-5 pr-4 ml-4"
+						title={
+							<span>
+								<i className="fa fa-user fa-fw"></i>Profile
+							</span>
+						}
+					>
+						<NavDropdown.Item className="navDropItem">
+							<Link to="/profile" className="profileSetting">
+								<i className="fas fa-envelope fa-fw"></i> User Profile
+							</Link>
+						</NavDropdown.Item>
+						{/* <NavDropdown.Item className="navDropItem">
               <Link to="/bookmark">
                 <i className="fas fa-bookmark mr-2"></i>Bookmark
               </Link>
             </NavDropdown.Item> */}
+						<NavDropdown.Item className="navDropItem">
+							<Link to="/history" className="historyButton">
+								<i className="far fa-clock mr-2"></i>History
+							</Link>
+						</NavDropdown.Item>
+						<NavDropdown.Item className="navDropItem" onClick={logOut}>
+							<Link className="fas fa-sign-out-alt i-logout mr-2"></Link>
+							Logout
+						</NavDropdown.Item>
+					</NavDropdown>
+				</>
+			);
+		} else {
+			setData();
+			setViewLogin(
+				<Nav>
+					<Link to="/login">
+						<Button className="signInButton   mr-5">
+							Sign In
+							<i className="fas fa-sign-in-alt ml-2"></i>
+						</Button>
+					</Link>
+				</Nav>
+			);
+		}
+	}, [props.viaLogin]);
 
-            <NavDropdown.Item className="navDropItem">
-              <i className="far fa-clock mr-2"></i>History
-            </NavDropdown.Item>
-            <NavDropdown.Item className="navDropItem" onClick={logOut}>
-              <Link className="fas fa-sign-out-alt i-logout mr-2"></Link>
-              Logout
-            </NavDropdown.Item>
-          </NavDropdown>
-        </>
-      );
-    } else {
-      setData();
-      setViewLogin(
-        <Nav>
-          <Link to="/login">
-            <Button className="signInButton   mr-5">
-              Sign In
-              <i className="fas fa-sign-in-alt ml-2"></i>
-            </Button>
-          </Link>
-        </Nav>
-      );
-    }
-  }, [props.viaLogin]);
-
-  return (
-    <Navbar bg="white" variant="light" expand="lg" className="navbar fixed-top">
-      <Link to="/" className="ml-3 brand">
-        <img src={Logo} alt="logo" />
-      </Link>
-      <Navbar.Toggle aria-controls="toogle" />
-      <Navbar.Collapse id="toogle">
-        <Nav className="ml-auto">
-          {data}
-          <Link to="/event/create">
-            <Button className="signInButton mr-3">
-              <i className="fas fa-plus mr-2 "></i>
-              Create Events
-            </Button>
-          </Link>
-          <Nav className="ml-auto">{viewLogin}</Nav>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+	return (
+		<Navbar bg="white" variant="light" expand="lg" className="navbar fixed-top">
+			<Link to="/" className="ml-3 brand">
+				<img src={Logo} alt="logo" />
+			</Link>
+			<Navbar.Toggle aria-controls="toogle" />
+			<Navbar.Collapse id="toogle">
+				<Nav className="ml-auto">
+					{data}
+					<Link to="/event/create">
+						<Button className="signInButton mr-3">
+							<i className="fas fa-plus mr-2 "></i>
+							Create Events
+						</Button>
+					</Link>
+					<Nav className="ml-auto">{viewLogin}</Nav>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 const mapStateToProps = (state) => {
-  return {
-    viaLogin: state.login.viaLogin,
-  };
+	return {
+		viaLogin: state.login.viaLogin,
+	};
 };
 
 const mapDispatchToProps = { logout };
--- src/pages/Home/Trend/Trend.js
+++ src/pages/Home/Trend/Trend.js
@@ -4,14 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar, faClock } from "@fortawesome/free-solid-svg-icons";
 
 import {
-  Container,
-  Row,
-  Col,
-  Jumbotron,
-  Button,
-  Form,
-  Card,
-  CardDeck,
+	Container,
+	Row,
+	Col,
+	Jumbotron,
+	Button,
+	Form,
+	Card,
+	CardDeck,
 } from "react-bootstrap";
 
 import axios from "axios";
@@ -20,94 +20,95 @@ import { Link } from "react-router-dom";
 import "./Trend.css";
 
 const Trend = () => {
-  const [data, setData] = useState([]);
+	const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const URL = "https://api.indrakawasan.com/event/show";
+	useEffect(() => {
+		const URL = "https://api.indrakawasan.com/event/show";
 
-    axios
-      .get(URL)
-      .then((res) => {
-        const data = res.data.slice(1, 5);
-        setData(data);
-      })
-      .catch((err) => {
-        if (
-          err &&
-          err.response &&
-          err.response.data &&
-          err.response.data.message
-        ) {
-          alert(err.response.data.message);
-        } else {
-          alert("Sorry we have server problem , Please wait.. ");
-        }
-      });
-  }, []);
+		axios
+			.get(URL)
+			.then((res) => {
+				const data = res.data.slice(1, 5);
+				setData(data);
+			})
+			.catch((err) => {
+				if (
+					err &&
+					err.response &&
+					err.response.data &&
+					err.response.data.message
+				) {
+					alert(err.response.data.message);
+				} else {
+					alert("Sorry we have server problem , Please wait.. ");
+				}
+			});
+	}, []);
 
-  const showTrend = data.map((item, index) => {
-    const URL = "https://api.indrakawasan.com/";
-    return (
-      <Col md={3} sm={12} className="my-2 mt-5 pt-2 pl-0 pr-0" key={index}>
-        <Card
-          border="secondary"
-          className="main-card"
-          key={index}
-          style={{ height: "100%" }}
-        >
-          <Card.Img variant="top" src={`${URL}${item.imageEvent}`} />
-          <Card.Body>
-            <Card.Text>{item.title}</Card.Text>
-            <Card.Text>
-              <FontAwesomeIcon icon={faCalendar} /> {item.date}
-            </Card.Text>
-            <Card.Text>
-              <FontAwesomeIcon icon={faClock} /> {item.time}
-            </Card.Text>
-            <Card.Text>
-              <i className="fas fa-map-marker-alt mr-2"></i>
-              {item.location}
-            </Card.Text>
-            <Link to={`/event/${item.id}`}>
-              <Button block variant="outline-danger">
-                See More
-              </Button>
-            </Link>
-          </Card.Body>
-        </Card>
-      </Col>
-    );
-  });
+	const showTrend = data.map((item, index) => {
+		const URL = "https://api.indrakawasan.com/";
+		return (
+			<Col md={3} sm={12} className="my-2 mt-5 pt-2 pl-0 pr-0" key={index}>
+				<Card
+					border="secondary"
+					className="main-card"
+					key={index}
+					style={{ height: "100%" }}
+				>
+					<Card.Img variant="top" src={`${URL}${item.imageEvent}`} />
+					<Card.Body>
+						<Card.Text>{item.title}</Card.Text>
+						<Card.Text>
+							<FontAwesomeIcon icon={faCalendar} /> {item.date}
+						</Card.Text>
+						<Card.Text>
+							<FontAwesomeIcon icon={faClock} /> {item.time}
+						</Card.Text>
+						<Card.Text>
+							<i className="fas fa-map-marker-alt mr-2"></i>
+							{item.location}
+						</Card.Text>
+						<Link
+							to={`/event/${item.id}`}
+							className="btn btn-outline-danger btn-block"
+						>
+							See More
+						</Link>
+					</Card.Body>
+				</Card>
+			</Col>
+		);
+	});
 
-  return (
-    <div>
-      <Jumbotron fluid className="trend-background">
-        <Container>
-          <h1 className="text-center mb-4 hostEvent">Host Your Own Events</h1>
-          <Form inline>
-            <Button
-              className="buttonCreate both-line-light mx-auto"
-              href="/event/create"
-            >
-              Create Events
-            </Button>
-          </Form>
-        </Container>
-      </Jumbotron>
+	return (
+		<div>
+			<Jumbotron fluid className="trend-background">
+				<Container>
+					<h1 className="text-center mb-4 hostEvent">Host Your Own Events</h1>
+					<Form inline>
+						<Button
+							className="buttonCreate both-line-light mx-auto"
+							href="/event/create"
+						>
+							Create Events
+						</Button>
+					</Form>
+				</Container>
+			</Jumbotron>
 
-      <Container className="mb-5">
-        <Row>
-          <h2 className="mb-2 mt-5 ml-5">TOP TRENDINGS EVENTS</h2>
-        </Row>
-        <Row>
-          <div className="underlineTrend mb-2 ml-5"></div>
-        </Row>
-        <Row>
-          <CardDeck>{showTrend}</CardDeck>
-        </Row>
-      </Container>
-    </div>
-  );
+			<Container className="mb-5">
+				<Row>
+					<h2 className="mb-2 mt-5 ml-5">TOP TRENDINGS EVENTS</h2>
+				</Row>
+				<Row>
+					<div className="underlineTrend mb-2 ml-5"></div>
+				</Row>
+				<Row>
+					<CardDeck>{showTrend}</CardDeck>
+				</Row>
+			</Container>
+		</div>
+	);
 };
 
 export default Trend;
