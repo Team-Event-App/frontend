7636485f8559b8f21122b5fabb7907bd2d85e52d
arb712 <ariqbimantoro@gmail.com>
2020-05-22 11:24:16 +0700

    profile update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { logout } from "../../actions/loginActions";
 import { connect } from "react-redux";
-
-import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
+import Navlinks from './Navlinks';
+import { Navbar, Nav, Button } from "react-bootstrap";
 
 import "./Navbar.css";
 import Logo from "./../../image/logo.png";
@@ -13,72 +13,18 @@ import Logo from "./../../image/logo.png";
 const Navbars = (props) => {
 	const [data, setData] = useState();
 	const [viewLogin, setViewLogin] = useState();
-	const history = useHistory();
-	const [user, setUser] = useState([]);
+
+
 	const URL = "https://api.indrakawasan.com/user/show";
 
-	const logOut = () => {
-		props.logout();
-		history.push("/");
-	};
+
 
 	useEffect(
 		() => {
-			axios
-				.get(URL)
-				.then((res) => {
-					const user = res.data;
-					setUser(user);
-				})
-				.catch((err) => {
-					if (
-						err &&
-						err.response &&
-						err.response.data &&
-						err.response.data.message
-					) {
-						alert(err.response.data.message);
-					} else {
-						alert("Sorry , can't get the data from server..");
-					}
-				});
-
 			if (props.viaLogin) {
 				setViewLogin(
 					<>
-						<NavDropdown
-							className="mr-5 pr-4 ml-4"
-							title={
-								<span>
-									<i className="fa fa-user fa-fw"></i>Profile
-								</span>
-							}
-						>
-							<NavDropdown.Item className="navDropItem">
-								<Link to="/profile" className="profileSetting">
-									<i className="fas fa-envelope fa-fw"></i> User Profile
-								</Link>
-							</NavDropdown.Item>
-
-							{/* <NavDropdown.Item className="navDropItem">
-									<Link to="/bookmark">
-										<i className="fas fa-bookmark mr-2"></i>Bookmark
-									</Link>
-								</NavDropdown.Item> */}
-
-							<NavDropdown.Item className="navDropItem">
-								<Link to={"/history"} className="profileSetting">
-									<i className="far fa-clock mr-2"></i>History
-								</Link>
-							</NavDropdown.Item>
-							<NavDropdown.Item className="navDropItem" onClick={logOut}>
-								<Link
-									to="#"
-									className="fas fa-sign-out-alt i-logout mr-2"
-								></Link>
-								Logout
-							</NavDropdown.Item>
-						</NavDropdown>
+					<Navlinks/>
 					</>
 				);
 			} else {
