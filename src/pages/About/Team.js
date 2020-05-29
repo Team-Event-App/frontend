23ff44f3c8e80ea873f08325ccd29ccbb3cd7dae
Indraacan <9haanacan@gmail.com>
2020-05-29 15:22:34 +0700

    add new

--- src/pages/About/Team.js
+++ src/pages/About/Team.js
@@ -16,24 +16,14 @@ class Team extends Component {
 		return (
 			<div>
 				<Container>
-					<Row>
+					<Row className="mb-3">
 						<Col>
 							<h2 className="text-center mt-3">Our Team</h2>
 							<div className="underlineAbout mx-auto"></div>
 						</Col>
 					</Row>
 
-					<Row className="mt-5 mb-5 text-center">
-						<Col md={4} sm={12}>
-							<img src={Evan} alt="Evan" className="rounded-image" />
-						</Col>
-						<Col md={4} sm={12}>
-							<img src={Indra} alt="" className="rounded-image" />
-						</Col>
-						<Col md={4} sm={12}>
-							<img src={Ariq} alt="" className="rounded-image ariqImg" />
-						</Col>
-					</Row>
+					
 				</Container>
 
 				<Container>
