798fc426dabf67a2e43d487e37732f362596c218
EvanCharles16 <evancharles1609@gmail.com>
2020-06-04 00:54:50 +0700

    fix bug login

--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -1,19 +1,24 @@
 import axios from "axios";
+import { createBrowserHistory } from "history";
 
 // const url = `${process.env.REACT_APP_API_URL}`;
 
+const history = createBrowserHistory();
+
 export const login = (data, match) => {
-  console.log(match);
   return async (dispatch) => {
     try {
       const response = await axios.post(
         `https://api.indrakawasan.com/user/login`,
         data
       );
+
       dispatch({
         type: "LOGIN_SUCCESS",
         payload: response.data,
       });
+      history.push("/");
+      window.location.reload();
     } catch (error) {
       dispatch({
         type: "LOGIN_FAILED",
