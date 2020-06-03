c422b81948446eaefdc835efa56fc555ebe4bcd6
EvanCharles16 <evancharles1609@gmail.com>
2020-06-04 00:25:09 +0700

    form login & regis , validation create event , alert Login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import "@fortawesome/react-fontawesome";
-
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import reducers from "./reducers";
+import { connect } from "react-redux";
 
 import Main from "./pages/Home/Main/Main";
 import CreateEvent from "./pages/Create_Event/EventCreate";
@@ -22,31 +18,35 @@ import ShowAll from "./pages/Show_All/ShowAll";
 import Profile from "./pages/Profile/Profile";
 import History from "./pages/History/HistoryEvent";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+import Modals from "./components/Modals/Modals";
 
-store.subscribe(() => {
-	console.log(store.getState());
-});
+const App = (props) => {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/showall" component={ShowAll} />
+          <Route path="/event/create">
+            {props.viaLogin ? <CreateEvent /> : <Login />}
+          </Route>
 
-const App = () => {
-	return (
-		<Provider store={store}>
-			<Router>
-				<Switch>
-					<Route path="/login" component={Login} />
-					<Route path="/register" component={Register} />
-					<Route path="/about" component={About} />
-					<Route path="/contact" component={Contact} />
-					<Route path="/showall" component={ShowAll} />
-					<Route path="/event/create" component={CreateEvent} />
-					<Route path="/event/:id" component={Detail} />
-					<Route path="/profile" component={Profile} />
-					<Route path="/history" component={History} />
-					<Route exact path="/" component={Main} />
-				</Switch>
-			</Router>
-		</Provider>
-	);
+          <Route path="/event/:id" component={Detail} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/history" component={History} />
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+      <Modals />
+    </>
+  );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return { viaLogin: state.login.viaLogin };
+};
+
+export default connect(mapStateToProps)(App);
--- src/reducers/loginReducers.js
+++ src/reducers/loginReducers.js
@@ -1,26 +1,43 @@
 const initialState = {
-	viaLogin: localStorage.getItem("access-token"),
-	loading: false,
+  viaLogin: localStorage.getItem("access-token"),
+  loading: false,
+  errorMessage: "",
+  isShow: false,
 };
 
 const login = (state = initialState, action) => {
-	switch (action.type) {
-		case "LOGOUT_SUCCESS":
-			localStorage.removeItem("access-token");
-			return {
-				...state,
-				viaLogin: "",
-			};
+  switch (action.type) {
+    case "LOGOUT_SUCCESS":
+      localStorage.removeItem("access-token");
+      return {
+        ...state,
+        viaLogin: "",
+      };
 
-		case "LOGIN_SUCCESS":
-			localStorage.setItem("access-token", action.payload);
-			return {
-				...state,
-				viaLogin: action.payload,
-			};
-		default:
-			return state;
-	}
+    case "LOGIN_SUCCESS":
+      localStorage.setItem("access-token", action.payload);
+      return {
+        ...state,
+        isShow: false,
+        viaLogin: action.payload,
+      };
+
+    case "LOGIN_FAILED":
+      return {
+        ...state,
+        isShow: true,
+        errorMessage: "Either your email or password is wrong.",
+      };
+
+    case "HIDE_MODAL":
+      return {
+        ...state,
+        isShow: false,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default login;
