302959cf649cea18a00c6e84c02fb4a483083590
arb712 <ariqbimantoro@gmail.com>
2020-06-11 14:16:33 +0700

    update page search not found

--- src/pages/Search/SearchCat.js
+++ src/pages/Search/SearchCat.js
@@ -8,13 +8,13 @@ import {
 	// Button,
 } from "react-bootstrap";
 import axios from "axios";
-
-import { connect } from 'react-redux'
-import { showError } from '../../actions/modalActions'
-
+import { useHistory } from "react-router-dom";
 import Card from "../../components/Card/Card";
+
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import { connect } from "react-redux";
+import { showError } from "./../../actions/modalActions";
 import "./../Home/Main/Main.css";
 import "./SearchCat.css";
 
@@ -23,9 +23,9 @@ import "./SearchCat.css";
 const ShowAll = (props) => {
 	const [data, setData] = useState([]);
 	const [search, setSearch] = useState([]);
-
+	const history = useHistory();
 	const params = new URLSearchParams(props.location.search);
-	const searchQuery = params.get("searchh");
+	const searchQuery = params.get("search");
 
 	// show data when user go to page
 	useEffect(() => {
@@ -42,13 +42,12 @@ const ShowAll = (props) => {
 					err.response.data &&
 					err.response.data.message
 				) {
-					props.showError(err.response.data.message);
+					alert(err.response.data.message);
 				} else {
-					props.showError("Sorry we have server problem , Try again later.. ");
+					alert("Sorry we have server problem , Try again later.. ");
 				}
 			});
-	}, [props]);
-
+	}, []);
 	useEffect(() => {
 		const URL2 = `https://api.indrakawasan.com/event/getCategory?search=${searchQuery}`;
 
@@ -64,27 +63,26 @@ const ShowAll = (props) => {
 					err.response.data &&
 					err.response.data.message
 				) {
-					props.showError(err.response.data.message);
+					alert(err.response.data.message);
 				} else {
-					props.showError("Sorry we have server problem , Try again later.. ");
+					alert("Sorry we have server problem , Try again later.. ");
 				}
 			});
-	}, [searchQuery, props]);
+	}, [searchQuery]);
 
 	const showAllEvent = data.map((data) => {
 		return (
 			<Col
-				key={data.id}
-				lg={3}
-				md={6}
-				sm={12}
-				className="my-2 mt-5 pt-2 pl-0 pr-0"
-			>
-				<Card item={data} />
-			</Col>
+			key={data.id}
+			lg={3}
+			md={6}
+			sm={12}
+			className="my-2 mt-5 pt-2 pl-0 pr-0"
+		>
+			<Card item={data} />
+		</Col>
 		);
 	});
-
 	const showAllSearch = search.map((data) => {
 		return (
 			<Col
@@ -98,40 +96,31 @@ const ShowAll = (props) => {
 			</Col>
 		);
 	});
-
-	// const handleSubmit = (event, a, b) => {
-	// 	event.preventDefault();
-	// 	const newSearch = event.target[0].value;
-	// 	console.log(newSearch);
-	// 	if (newSearch === "" && newSearch === null && newSearch === undefined) {
-	// 		const url2 = `https://api.indrakawasan.com/event/show`;
-	// 		axios
-	// 			.get(url2)
-	// 			.then((res) => {
-	// 				setData(res.data);
-	// 			})
-	// 			.catch((err) => {
-	// 				if (
-	// 					err &&
-	// 					err.response &&
-	// 					err.response.data &&
-	// 					err.response.data.message
-	// 				) {
-	// 					props.showError(err.response.data.message);
-	// 				} else {
-	// 					props.showError("Sorry we have server problem , Try again later.. ");
-	// 				}
-	// 			});
-	// 	} else {
-	// 		const url = `https://api.indrakawasan.com/event/show/${newSearch}`;
-	// 		axios.get(url).then((res) => {
-	// 			setSearch(res.data);
-	// 		});
-	// 	}
-	// };
-
 	const showAll = searchQuery ? showAllSearch : showAllEvent;
+	
+	if(search.length === 0){
+		return (
+			<div>
+			<Navbar />
+			<Container className="mt-5 pt-5 mb-5">
+				<Row>
+					<Col>
+						<h2 className="text-center mt-3">DISCOVER EVENTS</h2>
+						<div className="underlineShowAll mx-auto mb-5"></div>
+					</Col>
+				</Row>
+				<Container>
+					<Row className="rowEvent">
+						<h1 className="text-center"><h3>404 search not found</h3>
+					<p className="mb-5">We are sorry but the category you are looking for does not exist.</p></h1>
+					</Row>
+				</Container>
+			</Container>
 
+			<Footer />
+		</div>
+		)
+	}
 	return (
 		<div>
 			<Navbar />
